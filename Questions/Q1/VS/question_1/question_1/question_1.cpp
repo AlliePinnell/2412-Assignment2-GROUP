4fdf5c6687d7734eb5db355e22a2fc9964ea93a7
AlliePinnell <x-rap@hotmail.ca>
2026-03-02 17:02:49 -0500

    question 1 - basically done

---
+++ Questions/Q1/VS/question_1/question_1/question_1.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef struct Node {
+    int value;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* Insert(Node*& root, int insertValue) {
+    if (root == nullptr)
+    {
+        root = new Node{ insertValue, nullptr, nullptr };
+        return root;
+    }
+    if (insertValue < root->value)
+    {
+        root->left = Insert(root->left, insertValue);
+    }
+    else if (insertValue > root->value)
+    {
+        root->right = Insert(root->right, insertValue);
+    }
+    return root;
+}
+
+Node* Search(Node* root, int searchValue)
+{
+    if (root == nullptr)
+    {
+        return nullptr;
+    }
+    if (root->value == searchValue)
+    {
+        return root;
+    }
+    if (searchValue < root->value) {
+        return Search(root->left, searchValue);
+    }
+    if (searchValue > root->value) {
+        return Search(root->right, searchValue);
+    }
+    return nullptr;
+}
+
+void traversePreOrder(Node* root) {
+    if (root == nullptr) {
+        return;
+    }
+    std::cout << root->value << " ";
+    traversePreOrder(root->left);
+    traversePreOrder(root->right);
+}
+
+void traverseInOrder(Node* root) {
+    if (root == nullptr) {
+        return;
+    }
+    traverseInOrder(root->left);
+    std::cout << root->value << " ";
+    traverseInOrder(root->right);
+}
+
+void traversePostOrder(Node* root) {
+    if (root == nullptr) {
+        return;
+    }
+    traversePostOrder(root->left);
+    traversePostOrder(root->right);
+    std::cout << root->value << " ";
+}
+
+int main() {
+    int firstSequence[7] = { 15, 10, 20, 8, 12, 17, 25 };
+    int secondSequence[7] = { 50, 30, 70, 20, 40, 60, 80 };
+
+    Node* firstRoot = nullptr;
+    Node* secondRoot = nullptr;
+
+    for (int num : firstSequence) {
+        firstRoot = Insert(firstRoot, num);
+    }//end foreach
+
+    for (int num : secondSequence) {
+        secondRoot = Insert(secondRoot, num);
+    }//end foreach
+
+
+    std::cout << " ----- InOrder Traversals ----- " << std::endl;
+    traverseInOrder(firstRoot);
+    std::cout << std::endl << std::endl;
+    traverseInOrder(secondRoot);
+    std::cout << std::endl << std::endl;
+
+    std::cout << " ----- PreOrder Traversals ----- " << std::endl;
+    traversePreOrder(firstRoot);
+    std::cout << std::endl << std::endl;
+    traversePreOrder(secondRoot);
+    std::cout << std::endl << std::endl;
+
+    std::cout << " ----- PostOrder Traversals ----- " << std::endl;
+    traversePostOrder(firstRoot);
+    std::cout << std::endl << std::endl;
+    traversePostOrder(secondRoot);
+    std::cout << std::endl << std::endl;
+}
